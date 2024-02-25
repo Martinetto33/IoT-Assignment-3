ca381288fb9e38b7a750c9a038b93f05bfd63c78
Alessio Lizza <alessio.lizza@studio.unibo.it>
2024-02-25 11:40:58 +0100

    Added web server and client

---
+++ RiverMonitoringService/src/main/java/webServer/RiverMonitoringServiceApplication.java
@@ -0,0 +1,50 @@
+package webServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class RiverMonitoringServiceApplication {
+	private int waterLevel = 0;				//indicates the height of the water
+	private int openingGatePercentage = 0;	//indicates the opening of the valve %
+	private String status = "a";			//indicates the status of the system
+    public static void main(String[] args) {
+      	SpringApplication.run(RiverMonitoringServiceApplication.class, args);
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/dashboard")
+    public int dashboard(@RequestParam(value = "level")int gateOpening) {
+		try {
+			this.openingGatePercentage = gateOpening;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return this.openingGatePercentage;
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/getLevel")
+    public int getLevel() {
+        return this.waterLevel;
+    }
+
+	@CrossOrigin(origins = "*")
+    @GetMapping("/getStatus")
+    public String getStatus() {
+        return this.status;
+    }
+
+	@CrossOrigin(origins = "*")
+    @GetMapping("/getGatePercentage")
+    public int getOpening() {
+        return this.openingGatePercentage;
+    }
+}
---
+++ RiverMonitoringService/src/test/java/webServer/RiverMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package webServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
