f43cf63778291c1982ae4429d7cc67c50bc673de
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-04-04 17:32:52 +0200

    feat: added finite state machine for the backend; refinements needed

---
+++ RiverMonitoringService/app/src/main/java/rivermonitoringservice/fsm/RiverMonitoringServiceFSM.java
@@ -0,0 +1,26 @@
+package rivermonitoringservice.fsm;
+
+import rivermonitoringservice.state.api.State;
+
+public class RiverMonitoringServiceFSM {
+    private State currentState;
+
+    public RiverMonitoringServiceFSM(final State initialState) {
+        this.currentState = initialState;
+        this.currentState.onEntry();
+    }
+
+    public State getCurrentState() {
+        return this.currentState;
+    }
+
+    public void changeState(final State newState) {
+        this.currentState.onExit();
+        this.currentState = newState;
+        this.currentState.onEntry();
+    }
+
+    public void handle() {
+        this.currentState.handle();
+    }
+}
